74a9d57b2925e9c3c4fefb1528c761bea28a5bdb
Kevin <kevo0018@colorado.edu>
2014-11-24 11:47:50 -0700

    remove hw to debug

--- cpp/pbr.h
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
-void display_vector(vector<int>& digits);
-
-void add_area_code(vector<int>& digits, int a, int b, int c);
--- homeworks/hw-hello-world/hello.cpp
+++
@@ -1,9 +0,0 @@
-//
-// hello.cpp
-//
-// Student name: 
-// Your email address:
-
-// write your program below here. You may have it print whatever text
-// you like.
-
