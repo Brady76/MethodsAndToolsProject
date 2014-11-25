3c69ae73ef127046954601f1a527876d7b78d8ca
Brady <brau7176@colorado.edu>
2014-11-24 18:25:13 -0700

    HelloWorld testfile updated, sort of, LOL

--- Lesson 1: Basic Structure of C++/L1:Assignment/HelloWorldTest.cpp
+++ Lesson 1: Basic Structure of C++/L1:Assignment/HelloWorldTest.cpp
@@ -1,1 +1,3 @@
-// implement a test file here
+#include HelloWorld.cpp //We might want to make our actual assignment files into headers later, this speeds up compilation time. Just for future reference.  
+//Also.. LOL, we can't test this file, it's cout statements, a pretty good test to see if it works or not would be for them to run it and see if it actually prints. 
+
