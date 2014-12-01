a5c064a3f609d35a4d59bdbd331a8863650caf8b
Kevin Vo <kevo0018@colorado.edu>
2014-12-01 15:35:28 -0700

    Delete HelloWorldTest.cpp

--- Lesson 1: Basic Structure of C++/L1:Assignment/HelloWorldTest.cpp
+++
@@ -1,3 +0,0 @@
-#include HelloWorld.cpp //We might want to make our actual assignment files into headers later, this speeds up compilation time. Just for future reference.  
-//Also.. LOL, we can't test this file, it's cout statements, a pretty good test to see if it works or not would be for them to run it and see if it actually prints. 
-
