ac6f25135bd254a1f29f842ebc89483de7575363
Brady <brau7176@colorado.edu>
2014-12-07 12:28:16 -0700

    L4 Assignment done, have to make sure the tests work

---
+++ Lesson 4.1: Intro to Statements/L4: Assignment/L4Assignment.cpp
@@ -0,0 +1,27 @@
+/*4.1 Assignment: Intro to Statements*/
+
+#include <iostream>
+using namespace std;
+
+//Use an if statement to determine if value x is even or odd, return true if it's even, odd if it's false
+bool evenorodd(int x){
+}
+
+//Use a for loop to make a summation function, a summation is similar to a factorial except you add, so the summation of 5 is equal to 5 + 4 + 3 + 2 + 1
+int summation(int n){
+}
+
+//Batman's theme song is moving to cpp! Create a while loop that prints "nah" while the iterator is less than or equal to 16 (starting at 0), afterwards print "batman!" 
+//Make sure there's a space between the nah's and the batman! (e.g. nahnahnah batman!"
+string batmanthemesong(int y){
+}
+
+/*This is for testing, don't touch the main function!*/
+int main(){
+	if(evenorodd(5) == false && evenorodd(6) == true)
+		cout << "evenorodd passes!" << endl;
+	if(summation(5) == 15) 
+		cout << "summation passes!" << endl;
+	if(batmanthemesong == "nahnahnahnahnahnahnahnahnahnahnahnahnahnahnahnah batman!") 
+		cout << "batmanthemesong passes!" << endl;
+}
